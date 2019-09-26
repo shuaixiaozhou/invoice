0dd6b3e212dc122dc86c89db2c10b507f154645c
周小帅 <shuaixiaozhou@gmail.com>
2019-09-26 23:16:49 +0800

    上传二维码页面

---
+++ pages/uploader/uploader.js
@@ -0,0 +1,32 @@
+Page({
+    data: {
+        files: [],
+        message:"请选择要上传的二维码"
+    },
+    chooseImage: function (e) {
+      if (this.data.files.length > 0) {
+        this.setData({
+          message:"最多只能上传一个二维码！"
+        })
+        return
+      }
+        var that = this;
+        wx.chooseImage({
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                that.setData({
+                    files: that.data.files.concat(res.tempFilePaths)
+                });
+            }
+        })
+   
+    },
+    previewImage: function(e){
+        wx.previewImage({
+            current: e.currentTarget.id, // 当前显示图片的http链接
+            urls: this.data.files // 需要预览的图片http链接列表
+        })
+    }
+});
